523dea2cb3f8bfdc1e0ea48d306f4391cd9f03e3
yoyo201626 <15014217605@163.com>
2023-10-25 01:33:44 -0700

    test346

--- internals/playwright-config/src/index.js
+++ internals/playwright-config/src/index.js
@@ -39,12 +39,6 @@ const Config = ({ testDir, port }) => ({
     /* Only on CI systems run the tests headless */
     headless: !!process.env.CI
   },
-  webServer: {
-    command: 'pnpm dev',
-    url: `http://localhost:${port}`,
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
   projects: [
     {
       name: 'chromium',
